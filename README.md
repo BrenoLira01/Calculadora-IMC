eeb1e111783916e1b313a2f0fdbc7a7a4fd4d241
BrenoLira01 <brenoczbreno@gmail.com>
2024-03-07 18:58:09 -0300

    Foi adicionado mit license, readme, ícones e alterações no estilo, cores e posições dos elementos.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Introdução
+
+Este projeto é uma calculadora IMC (Índice De Massa Corporal) ainda em andamento.
+
+## 🧠 Tecnologias Utilizadas
+
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E) ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+
+## 🔍 Fontes
+
+Os elementos adicionados ou modificados neste projeto foram obtidos de diversas fontes. Abaixo está a lista de onde cada elemento foi encontrado:
+
+- **Ícone de Índice De Massa Corporal**: Fonte: <a href="https://www.flaticon.com/br/icones-gratis/bmi" title="bmi ícones">Bmi ícones criados por Freepik - Flaticon</a>
+- **Ícone de Lista**: Fonte: <a href="https://www.flaticon.com/br/icones-gratis/lista" title="lista ícones">Lista ícones criados por Freepik - Flaticon</a>
+- **Ícone de github**: Fonte: <a href="https://www.flaticon.com/br/icones-gratis/github" title="github ícones">Github ícones criados por Pixel perfect - Flaticon</a>
+- **Ícone de linkedin**: Fonte: <a href="https://www.flaticon.com/br/icones-gratis/linkedin" title="linkedin ícones">Linkedin ícones criados por Freepik - Flaticon</a>
+
+Agradeço aos criadores originais por disponibilizarem esses recursos.
--- script/script.js
+++ script/script.js
@@ -1,58 +1,94 @@
-let dados = [];
+try {
+  let dados = [];
 
-let enviar = document.querySelector(".enviar");
-let limparBtn = document.querySelector(".limpar");
+  document.addEventListener("DOMContentLoaded", function () {
+    let enviar = document.getElementById("enviar");
+    enviar.addEventListener("click", adicionar);
+  });
 
-enviar.addEventListener("click", adicionar);
+  function adicionar() {
+    let input1 = parseFloat(
+      document.querySelector("#primeiroInput").value.replace(/,/g, ".")
+    );
+    let input2 = parseFloat(document.querySelector("#segundoInput").value);
 
-function adicionar() {
-  let input1 = parseFloat(
-    document.querySelector(".primeiroInput").value.replace(/,/g, ".")
-  );
-  let input2 = parseFloat(document.querySelector(".segundoInput").value);
+    console.log(input1);
+    console.log(input2);
 
-  dados.push(input1, input2);
+    if (isNaN(input1) || isNaN(input2)) {
+      let descricaoElements = document.querySelectorAll(".descricao");
+      descricaoElements.forEach((element) => {
+        element.style.color = "red";
+      });
+    } else {
+      dados.push(input1, input2);
+      calcular();
+    }
+  }
 
-  calcular();
-}
+  function calcular() {
+    let valorAltura = dados[0];
+    let valorPeso = dados[1];
+    let imc = valorPeso / (valorAltura * valorAltura);
+    let classificacao = document.getElementById("classificacao");
 
-function calcular() {
-  let valorAltura = dados[0];
-  let valorPeso = dados[1];
-  let imc = valorPeso / (valorAltura * valorAltura);
-
-  document.querySelector(".caixaResultado").style.display = "flex";
-
-  if (isNaN(imc)) {
-    document.getElementById("valor").innerText = "0";
-  } else {
+    document.querySelector(".caixaResultado").style.display = "flex";
     document.getElementById("valor").innerText = imc.toFixed(2);
+
+    let descricaoElements = document.querySelectorAll(".descricao");
+    descricaoElements.forEach((element) => {
+      element.style.color = "black";
+    });
+
+    console.log(dados);
+
+    if (imc < 18.5 || imc >= 30) {
+      classificacao.style.color = "red";
+    } else {
+      classificacao.style.color = "black";
+    }
+
+    if (imc < 18.5) {
+      classificacao.innerText = "Magreza";
+    } else if (imc >= 18.5 && imc <= 24.9) {
+      classificacao.innerText = "Normal";
+    } else if (imc >= 25 && imc <= 29.9) {
+      classificacao.innerText = "Sobrepeso";
+    } else if (imc >= 30 && imc <= 39.9) {
+      classificacao.innerText = "Obesidade";
+    } else if (imc >= 40) {
+      classificacao.innerText = "Obesidade Grave";
+    }
   }
+
+  let limparBtn = document.getElementById("limpar");
+  limparBtn.addEventListener("click", limpar);
+
+  function limpar() {
+    document.querySelector("#primeiroInput").value = "";
+    document.querySelector("#segundoInput").value = "";
+    document.querySelector("#valor").innerText = "0";
+    document.querySelector("#classificacao").innerText = "";
+    dados = [];
+  }
+
+  var primeiroInput = document.querySelector("#primeiroInput");
+  var segundoInput = document.querySelector("#segundoInput");
+  var enviarBotao = document.querySelector("#enviar");
+
+  primeiroInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      segundoInput.focus();
+    }
+  });
+
+  segundoInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      enviarBotao.focus();
+    }
+  });
+} catch (error) {
+  console.error("Ocorreu um erro:", error);
 }
-
-limparBtn.addEventListener("click", limpar);
-
-function limpar() {
-  document.querySelector(".primeiroInput").value = "";
-  document.querySelector(".segundoInput").value = "";
-  document.getElementById("valor").innerText = "0";
-  dados = [];
-}
-
-var primeiroInput = document.querySelector(".primeiroInput");
-var segundoInput = document.querySelector(".segundoInput");
-var enviarBotao = document.querySelector(".enviar");
-
-primeiroInput.addEventListener("keydown", function (event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    segundoInput.focus();
-  }
-});
-
-segundoInput.addEventListener("keydown", function (event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    enviarBotao.focus();
-  }
-});
